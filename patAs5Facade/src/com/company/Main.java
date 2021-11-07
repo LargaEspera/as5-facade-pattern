9e522769eea718db399004f506990e056b9ad16c
LargaEspera <NONAME3253@gmail.com>
2021-11-08 01:20:02 +0600

    as5-facade-pattern

---
+++ patAs5Facade/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        OrderFacade order1 = new OrderFacade();
+
+        order1.executeOrder();
+    }
+}
